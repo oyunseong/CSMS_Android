b8065868ffc5984e520009de955edb3c3367c1e0
oyunseong <gkrlsanjf@naver.com>
2024-06-17 14:08:55 +0900

    [Feat] logger 유틸 함수 추가
    - 빠르게 로그를 찍을 수 있는 전역 함수 추가

--- app/src/main/java/com/verywords/csms_android/utils/Logger.kt
+++ app/src/main/java/com/verywords/csms_android/utils/Logger.kt
@@ -3,10 +3,6 @@ package com.verywords.csms_android.utils
 import android.util.Log
 
 
-fun String.log(tag: String = "fastLog") {
-    Log.d(tag, this)
-}
-
 fun log(tag: String = "fastLog", message: String) {
     Log.d(tag, message)
 }
