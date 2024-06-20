2f0e5d3dd244ae057aff8d6714a7f10a3a6dc5f2
oyunseong <gkrlsanjf@naver.com>
2024-06-20 15:58:50 +0900

    [Feat] Navigation 기능 추가

---
+++ app/src/main/java/com/verywords/Navigation.kt
@@ -0,0 +1,14 @@
+package com.verywords
+
+object Navigation {
+
+    object Args {
+        const val USER_ID = "user_id"
+    }
+
+    object Routes {
+        const val HomeScreen = "HomeScreen"
+        const val DatabaseScreen = "DatabaseScreen"
+    }
+
+}
--- app/src/main/java/com/verywords/csms_android/NavHostScreen.kt
+++ app/src/main/java/com/verywords/csms_android/NavHostScreen.kt
@@ -2,12 +2,54 @@ package com.verywords.csms_android
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.verywords.Navigation
 import com.verywords.csms_android.ui.screen.home.HomeScreen
+import com.verywords.csms_android.ui.screen.room.DatabaseScreen
 
 // TODO 네비게이션 기능 필요시 추가 예정.
 @Composable
 fun NavHostScreen(
     modifier: Modifier = Modifier
 ) {
-    HomeScreen(modifier=modifier)
+    val navController = rememberNavController()
+
+    fun clearAndNavigate(route: String) {
+        navController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(0) { inclusive = true }
+        }
+    }
+
+    fun popUp() {
+        navController.popBackStack()
+    }
+
+    fun navigate(route: String) {
+        navController.navigate(route) { launchSingleTop = true }
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = Navigation.Routes.HomeScreen
+    ) {
+        composable(
+            route = Navigation.Routes.HomeScreen
+        ) {
+            HomeScreen(
+                modifier = modifier,
+                navigate = ::navigate
+            )
+        }
+
+        composable(
+            route = Navigation.Routes.DatabaseScreen
+        ) {
+            DatabaseScreen(
+                modifier = modifier,
+            )
+        }
+    }
 }
